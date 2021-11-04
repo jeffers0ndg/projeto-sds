6a77cc5d08c346889ff15aa1fb7dd3a1bf64d8e5
Jefferson Dantas <jeffers0ndantasg@gmail.com>
2021-11-04 19:16:52 -0300

    Group by search

--- backend/src/main/java/com/jefferson/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/jefferson/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.jefferson.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.jefferson.dsvendas.dto.SaleSuccessDTO;
+import com.jefferson.dsvendas.dto.SaleSumDTO;
 import com.jefferson.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query("SELECT new com.jefferson.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupeBySeller();
+	
+	@Query("SELECT new com.jefferson.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), "
+			+ "SUM(obj.deals)) FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupeBySeller();
 
 }
